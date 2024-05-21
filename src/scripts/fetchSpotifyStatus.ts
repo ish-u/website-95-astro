83456e73d1ce48a02e4ccd13478b46b3ce39c155
Anmol Gupta <anmolgupta520@gmail.com>
2024-05-22 00:30:19 +0530

    update currently-playing url

--- src/scripts/fetchSpotifyStatus.ts
+++ src/scripts/fetchSpotifyStatus.ts
@@ -1,24 +1,29 @@
 // Fetch Now Playing Song on Spotify
 export async function fetchSongDetails() {
   // Song Name
-  const song = await (
-    await fetch("https://spotify-readme.ishu2.repl.co/nowPlaying/plainText")
-  ).json();
-  const title = song.name;
-  const artists = song.artists.join(", ");
+  const song = await fetch(
+    "https://currently-playing.up.railway.app/currently-playing"
+  )
+    .then((res) => res.json())
+    .catch((e) => console.error(e));
+  const sonegURLElements = document.getElementsByClassName("song-url");
   const sonegElements = document.getElementsByClassName("song-name");
   const songImageElements = document.getElementsByClassName("song-image");
   for (let i = 0; i < sonegElements.length; i++) {
     const songNameEle = sonegElements.item(i);
     if (songNameEle) {
-      songNameEle.innerHTML = title + " - " + artists;
+      songNameEle.innerHTML = song?.name ?? "Nothing";
     }
-    // Song Image
+
+    const songUrl = sonegURLElements.item(i);
+    if (songUrl) {
+      (songUrl as HTMLAnchorElement).href = song?.url ?? "";
+    }
+
     const songImageEle = songImageElements.item(i);
     if (songImageEle) {
       (songImageEle as HTMLImageElement).src =
-        "https://spotify-readme.ishu2.repl.co/nowPlaying/image?t=" +
-        new Date().getMilliseconds();
+        song?.image ?? "images/music.png";
     }
   }
 }
